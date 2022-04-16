11ed40fadfb3490205c9447774c9264d8e22ffd9
karna <karna_raj@yahoo.com>
2022-04-16 09:55:06 +0530

    added scroll

--- src/Pages/Components/Navbar/Navbar.js
+++ src/Pages/Components/Navbar/Navbar.js
@@ -9,133 +9,143 @@ import { Link as LNK } from "react-scroll";
 import { useState } from "react";
 
 export default function NaviBar() {
-  const [header, setHeader] = useState(false);
+    const [header, setHeader] = useState(false);
 
-  const changeBackground = () => {
-    if (window.scrollY >= 10) {
-      setHeader(true);
-    } else {
-      setHeader(false);
-    }
-  };
+    const changeBackground = () => {
+        if (window.scrollY >= 10) {
+            setHeader(true);
+        } else {
+            setHeader(false);
+        }
+    };
 
-  window.addEventListener("scroll", changeBackground);
-  const [isOpen, setOpen] = useState(false);
+    window.addEventListener("scroll", changeBackground);
+    const [isOpen, setOpen] = useState(false);
 
-  const [click, setClick] = useState(false);
+    const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
-  return (
-    <nav className="navibar">
-      <div className={header ? "header active" : "header"}>
-        <div
-          className={`header-wrapper ${click ? "header-wrapper active" : ""} `}
-        >
-          <div className="nav-container">
-            <div className="logo-header">
-              <Link
-                onClick={() => scroll.scrollToTop()}
-                className="nav-links"
-                activeClassName="active"
-                to="/"
-              >
-                <img src={IEEE} className="nav-logo" alt="Murious" />
-              </Link>
+    const handleClick = () => setClick(!click);
+    return (
+        <nav className="navibar">
+            <div className={header ? "header active" : "header"}>
+                <div
+                    className={`header-wrapper ${
+                        click ? "header-wrapper active" : ""
+                    } `}
+                >
+                    <div className="nav-container">
+                        <div className="logo-header">
+                            <Link
+                                onClick={() => scroll.scrollToTop()}
+                                className="nav-links"
+                                activeClassName="active"
+                                to="/"
+                            >
+                                <img
+                                    src={IEEE}
+                                    className="nav-logo"
+                                    alt="Murious"
+                                />
+                            </Link>
+                        </div>
+                        <div className="header-links">
+                            <ul
+                                className={
+                                    click ? "nav-menu active" : "nav-menu"
+                                }
+                            >
+                                <li className="nav-item">
+                                    <LNK
+                                        to="about-website"
+                                        className="nav-links"
+                                        activeClassName="active"
+                                        onClick={handleClick}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-60}
+                                        duration={100}
+                                    >
+                                        About
+                                    </LNK>
+                                </li>
+                                <li className="nav-item">
+                                    <LNK
+                                        to="schedule-section-heading"
+                                        className="nav-links"
+                                        activeClassName="active"
+                                        onClick={handleClick}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={100}
+                                    >
+                                        Schedule
+                                    </LNK>
+                                </li>
+                                <li className="nav-item">
+                                    <LNK
+                                        to="events-section"
+                                        className="nav-links"
+                                        activeClassName="active"
+                                        onClick={handleClick}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-80}
+                                        duration={100}
+                                    >
+                                        Events
+                                    </LNK>
+                                </li>
+                                <li className="nav-item">
+                                    <LNK
+                                        to="sponsors-murious"
+                                        className="nav-links"
+                                        activeClassName="active"
+                                        onClick={handleClick}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={50}
+                                        duration={1000}
+                                    >
+                                        Sponsors
+                                    </LNK>
+                                </li>
+                                <li className="nav-item">
+                                    <LNK
+                                        to="team-murious"
+                                        className="nav-links"
+                                        activeClassName="active"
+                                        onClick={handleClick}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={50}
+                                        duration={1000}
+                                    >
+                                        Team
+                                    </LNK>
+                                </li>
+                                <li className="nav-item contact">
+                                    <LNK
+                                        to="contact-form-murious"
+                                        className="nav-links"
+                                        activeClassName="active"
+                                        onClick={handleClick}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={0}
+                                        duration={1000}
+                                    >
+                                        Contact
+                                    </LNK>
+                                </li>
+                            </ul>
+                            <div className="nav-icon" onClick={handleClick}>
+                                {click ? <RiCloseLine /> : <RiMenu3Line />}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="header-links">
-              <ul className={click ? "nav-menu active" : "nav-menu"}>
-                <li className="nav-item">
-                  <LNK
-                    to="about"
-                    className="nav-links"
-                    activeClassName="active"
-                    onClick={handleClick}
-                    spy={true}
-                    smooth={true}
-                    offset={-60}
-                    duration={1000}
-                  >
-                    About
-                  </LNK>
-                </li>
-                <li className="nav-item">
-                  <LNK
-                    to="event-schedule-murious"
-                    className="nav-links"
-                    activeClassName="active"
-                    onClick={handleClick}
-                    spy={true}
-                    smooth={true}
-                    offset={-50}
-                    duration={1000}
-                  >
-                    Schedule
-                  </LNK>
-                </li>
-                <li className="nav-item">
-                  <LNK
-                    to="events-murious"
-                    className="nav-links"
-                    activeClassName="active"
-                    onClick={handleClick}
-                    spy={true}
-                    smooth={true}
-                    offset={80}
-                    duration={1000}
-                  >
-                    Events
-                  </LNK>
-                </li>
-                <li className="nav-item">
-                  <LNK
-                    to="sponsors-murious"
-                    className="nav-links"
-                    activeClassName="active"
-                    onClick={handleClick}
-                    spy={true}
-                    smooth={true}
-                    offset={50}
-                    duration={1000}
-                  >
-                    Sponsors
-                  </LNK>
-                </li>
-                <li className="nav-item">
-                  <LNK
-                    to="team-murious"
-                    className="nav-links"
-                    activeClassName="active"
-                    onClick={handleClick}
-                    spy={true}
-                    smooth={true}
-                    offset={50}
-                    duration={1000}
-                  >
-                    Team
-                  </LNK>
-                </li>
-                <li className="nav-item contact">
-                  <LNK
-                    to="contact-form-murious"
-                    className="nav-links"
-                    activeClassName="active"
-                    onClick={handleClick}
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration={1000}
-                  >
-                    Contact
-                  </LNK>
-                </li>
-              </ul>
-              <div className="nav-icon" onClick={handleClick}>
-                {click ? <RiCloseLine /> : <RiMenu3Line />}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 }
