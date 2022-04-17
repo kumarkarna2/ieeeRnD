9a6766e6f1eee2942a8fcc2fa23588e2ee11f6cf
karna <karna_raj@yahoo.com>
2022-04-17 17:16:45 +0530

    .

--- src/Pages/Components/About/About.js
+++ src/Pages/Components/About/About.js
@@ -6,20 +6,21 @@ import { Link } from "react-router-dom";
 import "./About.css";
 
 export default function About() {
-  return (
-    <div className="about-website" id="about">
-      <div className="about-us-box">
-        <div className="about-heading">About Us</div>
-        <div className="about-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum
-          tellus ex, vel egestas ipsum ornare ut. Cras ultrices sit amet lorem
-          eget pretium. Donec egestas ac urna sed consequat. Pellentesque eget
-          placerat mi. Etiam rhoncus nec ex at egestas. Vivamus laoreet massa
-          sit amet dolor dictum, at scelerisque turpis interdum. Maecenas massa
-          est, accumsan nec egestas eu, dapibus quis ligula. Nullam nisi urna,
-          tincidunt ac nunc eget, volutpat commodo metus.
+    return (
+        <div className="about-website" id="about">
+            <div className="about-us-box">
+                <div className="about-heading">ABOuT us</div>
+                <div className="about-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam dictum tellus ex, vel egestas ipsum ornare ut. Cras
+                    ultrices sit amet lorem eget pretium. Donec egestas ac urna
+                    sed consequat. Pellentesque eget placerat mi. Etiam rhoncus
+                    nec ex at egestas. Vivamus laoreet massa sit amet dolor
+                    dictum, at scelerisque turpis interdum. Maecenas massa est,
+                    accumsan nec egestas eu, dapibus quis ligula. Nullam nisi
+                    urna, tincidunt ac nunc eget, volutpat commodo metus.
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/Pages/Components/Events/Events.js
+++ src/Pages/Components/Events/Events.js
@@ -3,13 +3,13 @@ import "./Events.css";
 import EventsCard from "./Card/EventsCard";
 
 export default function Events() {
-  return (
-    <div className="events-section" id="events">
-      <div className="events-heading">Events</div>
-      <div className="events-cards-section">
-        <EventsCard
-          title="Event-1"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    return (
+        <div className="events-section" id="events">
+            <div className="events-heading">evENts</div>
+            <div className="events-cards-section">
+                <EventsCard
+                    title="Event-1"
+                    desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua."
                 />
                 <EventsCard
