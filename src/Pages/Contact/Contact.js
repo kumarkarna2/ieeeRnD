3b4c86760c483af458fa2d1f9438e3711d53bb5e
karna <karna_raj@yahoo.com>
2022-04-18 17:22:53 +0530

    added g form

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,48 +4,23 @@ import NaviBar from "../Components/Navbar/Navbar";
 import "./Contact.css";
 
 export default function Contact() {
-  return (
-    <div>
-      <NaviBar />
-      <div className="contact-us-section">
-        <div className="contact-us-heading">Contact Us</div>
-        <form className="contact-form">
-          <label className="fc-title" for="fname">
-            First name:
-          </label>
-          <input
-            placeholder="First Name"
-            className="input-boxes"
-            type="text"
-            id="fname"
-            name="fname"
-          />
-          <label className="fc-title" for="lname">
-            Last name:
-          </label>
-          <input
-            className="input-boxes"
-            placeholder="Last Name"
-            type="text"
-            id="lname"
-            name="lname"
-          />
-          <label className="fc-title" for="message">
-            Message:
-          </label>
-          <textarea
-            className="input-boxes"
-            name="message"
-            rows="5"
-            cols="30"
-            placeholder="Enter Your Message Here..."
-          />
-          <div className="submit-b">
-            <input type="submit" value="Submit" className="submit-button-cf" />
-          </div>
-        </form>
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <NaviBar />
+            <div className="contact-us-section">
+                <div className="contact-us-heading">Contact Us</div>
+                <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSeRxqTOSP-c1GkQJzmdO7LYbsMkCekrtqLn03184NDe7EQ9pQ/viewform?embedded=true"
+                    width="640"
+                    height="979"
+                    frameborder="0"
+                    marginheight="0"
+                    marginwidth="0"
+                >
+                    Loading…
+                </iframe>
+            </div>
+            <Footer />
+        </div>
+    );
 }
