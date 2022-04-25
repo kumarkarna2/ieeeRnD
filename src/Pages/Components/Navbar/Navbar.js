dd902a9ea24f20a5b785499aef32c5e462181a83
Karna Kumar Chaudhary <karna_raj@yahoo.com>
2022-04-25 17:32:49 +0530

    HJBJ

--- src/Pages/Components/Navbar/Navbar.js
+++ src/Pages/Components/Navbar/Navbar.js
@@ -48,14 +48,91 @@ export default function Navibar() {
                     </li>
                 </ul>
                 <ul id="link">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/">Contact Us</Link>
+                    <li><Link
+                        to="about"
+                        className="nav-links"
+                        activeClassName="active"
+                        // onClick={handleClick}
+                        spy={true}
+                        smooth={true}
+                        offset={-60}
+                        duration={100}
+                    >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="schedule-section-heading"
+                        className="nav-links"
+                        activeClassName="active"
+                        // onClick={handleClick}
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={100}
+                    >
+                        Schedule
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="events-section"
+                        className="nav-links"
+                        activeClassName="active"
+                        // onClick={handleClick}
+                        spy={true}
+                        smooth={true}
+                        offset={-80}
+                        duration={100}
+                    >
+                        Events
+                    </Link>
+                </li>
+                <li >
+                    <Link
+                        to="/sponsors"
+                        className="nav-links"
+                        activeClassName="active"
+                        // onClick={handleClick}
+                        // onClick={() => scroll.scrollToTop()}
+                        spy={true}
+                        smooth={true}
+                        offset={50}
+                        duration={100}
+                    >
+                        Sponsors
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/team"
+                        className="nav-links"
+                        activeClassName="active"
+                        // onClick={handleClick}
+                        // onClick={() => scroll.scrollToTop()}
+                        spy={true}
+                        smooth={true}
+                        offset={50}
+                        duration={100}
+                    >
+                        Team
+                    </Link>
+                </li>
+                <li> 
+                    <Link
+                        to="/contact"
+                        className="contact-button"
+                        activeClassName="active"
+                        // onClick={() => scroll.scrollToTop()}
+                        // onClick={handleClick}
+                        spy={true}
+                        smooth={true}
+                        offset={50}
+                        duration={100}
+                    >
+                        Contact US
+                        </Link>
                     </li>
                 </ul>
             </nav>
