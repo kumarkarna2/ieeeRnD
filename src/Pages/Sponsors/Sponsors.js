a82148fd26a26e2470c7dc5c0b7cdb7c92d5405a
Karna Kumar Chaudhary <karna_raj@yahoo.com>
2022-04-27 15:54:29 +0530

    updated sponsor

--- src/Pages/Sponsors/Sponsors.js
+++ src/Pages/Sponsors/Sponsors.js
@@ -2,17 +2,21 @@ import React from "react";
 import NaviBar from "../Components/Navbar/Navbar";
 import SponsorsCard from "./Cards/SponsorsCard";
 import "./Sponsors.css";
-import ace from "./Images/ace.png";
+import ace from "./Images/ac.png";
 import cl from "./Images/cl.png";
 import cp from "./Images/cp.png";
 import jb from "./Images/jb.png";
 import ta from "./Images/ta.png";
-import db from "./Images/db.png";
+import db from "./Images/db1.png";
 import vh from "./Images/vh.png";
 import sn from "./Images/sn.png";
 import sy from "./Images/sy.png";
 import cc from "./Images/cc.png";
 import ls from "./Images/ls.png";
+import ws from "./Images/ws.png";
+import nb from "./Images/nb.png";
+import ib from "./Images/ib.png";
+import es from "./Images/es.png";
 import Footer1 from "../Components/Footer1/Footer1";
 
 export default function Sponsors() {
@@ -39,6 +43,15 @@ export default function Sponsors() {
                         img={cp}
                         alt="Collage Pond"
                         spname="Collage Pond"
+                    />
+                </div>
+                <div className="sponsors-heading">Tech Partner</div>
+                <div className="sponsors-section">
+                    <SponsorsCard
+                        link="//wharfstreetstrategies.com/"
+                        img={ws}
+                        alt="WSS"
+                        spname="WSS"
                     />
                 </div>
                 <div className="sponsors-heading">Associate Partner</div>
@@ -78,6 +91,30 @@ export default function Sponsors() {
                         img={sn}
                         alt="startupnews"
                         spname="startupnews"
+                    />
+                    <SponsorsCard
+                        link="//www.noticebard.com/"
+                        img={nb}
+                        alt="Notice Board"
+                        spname="Notice Board"
+                    />
+                </div>
+                <div className="sponsors-heading">Interview Partner</div>
+                <div className="sponsors-section">
+                    <SponsorsCard
+                        link="//interviewbuddy.in/"
+                        img={ib}
+                        alt="Interview Buddy"
+                        spname="Interview Buddy"
+                    />
+                </div>
+                <div className="sponsors-heading">Education Partner</div>
+                <div className="sponsors-section">
+                    <SponsorsCard
+                        link="//easyshiksha.com/"
+                        img={es}
+                        alt="Easy Shiksha"
+                        spname="Easy Shiksha"
                     />
                 </div>
                 <div className="sponsors-heading">Streaming Partner</div>
